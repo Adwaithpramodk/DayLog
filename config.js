1795688df29b44c608f0f2c407f8b08a14910b3e
Adwaith Pramod K <adwaithpramod786@gmail.com>
2026-04-22 14:34:29 +0530

    DayLog Tracker

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - Keep this file out of Git!
+export const appwriteConfig = {
+    endpoint: 'https://sgp.cloud.appwrite.io/v1',
+    projectId: '69e877320008ecdec5b8' // Replace this with your actual ID
+};
